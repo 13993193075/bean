76d593e59bfcaff0cf8c755c717466ea1d044f60
unknown <1554561464@qq.com>
2026-01-11 20:00:31 +0800

    Enjoy Coding

--- src/GBT/index.js
+++ src/GBT/index.js
@@ -4,6 +4,7 @@ import gbt2260code6 from './gbt2260code6.js'
 import gbt2261 from './gbt2261.js'
 import gbt2659 from './gbt2659.js'
 import gbt3304 from './gbt3304.js'
+import gbt4658 from './gbt4658.js'
 
 export {
     gbt2260code2,
@@ -11,7 +12,8 @@ export {
     gbt2260code6,
     gbt2261,
     gbt2659,
-    gbt3304
+    gbt3304,
+    gbt4658
 }
 export default {
     gbt2260code2,
@@ -19,5 +21,6 @@ export default {
     gbt2260code6,
     gbt2261,
     gbt2659,
-    gbt3304
+    gbt3304,
+    gbt4658
 }
