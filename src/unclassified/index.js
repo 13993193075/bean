2e3884821ccd077f610cf70612c8f2a6a6b59303
unknown <1554561464@qq.com>
2026-02-01 15:41:05 +0800

    Enjoy Coding

--- src/unclassified/index.js
+++ src/unclassified/index.js
@@ -3,18 +3,21 @@ import deepClone from './deepClone.js'
 import random from './random.js'
 import regexp from './regexp.js'
 import sort from './sort.js'
+import tree from './tree.js'
 
 export {
     dateFormat,
     deepClone,
     random,
     regexp,
-    sort
+    sort,
+    tree
 }
 export default {
     dateFormat,
     deepClone,
     random,
     regexp,
-    sort
+    sort,
+    tree
 }
